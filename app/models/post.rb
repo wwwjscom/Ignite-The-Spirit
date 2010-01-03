2cf1704bcccebe2e4aad0492a12b4848ab677fec
Jason Soo <wwwjscom@gmail.com>
2010-01-03 13:17:15 -0600

    now display 3 recent news stories

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,10 @@
 class Post < ActiveRecord::Base
 
-  def self.recent
-    self.last
+  def self.recent(posts = 1)
+    if posts == 1
+      self.last
+    else
+      self.find(:all, :order => 'id DESC', :limit => posts)
+    end
   end
 end

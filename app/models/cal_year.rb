8e3a8e4c61540e53e0d31bc5b698f2e3fb928cb8
Jason Soo <wwwjscom@Jason-Soos-iMac.local>
2010-01-03 02:45:24 -0600

    dependent destroy

--- app/models/cal_year.rb
+++ app/models/cal_year.rb
@@ -1,5 +1,5 @@
 class CalYear < ActiveRecord::Base
-  has_many :bio
+  has_many :bio, :dependent => :destroy
 
   def bios
     Bio.find(:all, :conditions => ['cal_year_id = ?', self.id], :order => 'month_id')
